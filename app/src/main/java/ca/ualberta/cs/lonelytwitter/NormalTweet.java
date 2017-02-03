5569d5a78a7efe56268556ea7c7301dc854970ad
jbbela <belardo@ualberta.ca>
2017-02-02 22:36:40 -0700

    Update NormalTweet.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
@@ -7,19 +7,35 @@ import java.util.Date;
  */
 
 /**
- * Class of NormalTweet object that is used to hold a tweet's message and date; is not considered
+ * Subclass of Tweet that is used to hold a tweet's message and date; is not considered
  * important.
  * Inherits from Tweet class
  */
 public class NormalTweet extends Tweet {
+    /**
+    * Instantiates new NormalTweet; takes in string message and sets date as current date.
+    * 
+    * @param message
+    */
     public NormalTweet(String message) {
         super(message);
     }
-
+    
+    /**
+    * Instantiates new NormalTweet; takes in string message and sets date as given date.
+    *
+    * @param date
+    * @param message
+    */
     public NormalTweet(Date date, String message) {
         super(date, message);
     }
-
+    
+    /**
+    * Determines if tweet is important or not.
+    *
+    * @return false
+    **/
     @Override
     public Boolean isImportant() {
         return false;   // it is only a NormalTweet, so it is not important; return false
