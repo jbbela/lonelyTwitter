a22bf814685f0be179997a1ce1e8bc6b46a5af54
jbbela <belardo@ualberta.ca>
2017-02-02 22:23:33 -0700

    Update ImportantTweet.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
@@ -7,18 +7,29 @@ import java.util.Date;
  */
 
 /**
- * Class of ImportantTweet object that holds a tweet's message and date; is considered important
- * Inherits from the Tweet class
+ * Class of ImportantTweet object that holds a tweet's message and date; is considered important.
+ * Inherits from the Tweet class.
  */
 public class ImportantTweet extends Tweet {
+    /**
+    * Instance of new ImportantTweet. Given string is message; sets current date as date.
+    * @param message
+    */
     public ImportantTweet(String message) {
         super(message);
     }
 
+    /**
+    * Instance of new ImportantTweet. Given string is message; given date is set as date.
+    */
     public ImportantTweet(Date date, String message) {
         super(date, message);
     }
-
+    
+    /**
+    * Determines if tweet is important or not; always returns true.
+    * @return true
+    */
     @Override
     public Boolean isImportant() {
         return true;    // returns true if it is an ImportantTweet
